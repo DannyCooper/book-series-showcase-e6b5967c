d2a97c0c16f235011367821b4e0c037f779f8e84
Danny Cooper <email@dannycooper.com>
2026-02-12 13:48:45 +0000

    newsletter

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import BookDetail from "./pages/BookDetail";
+import Newsletter from "./pages/Newsletter";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/book/:slug" element={<BookDetail />} />
+          <Route path="/newsletter" element={<Newsletter />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/Newsletter.tsx
@@ -0,0 +1,14 @@
+import BookDetailNav from "@/components/book-detail/BookDetailNav";
+import NewsletterSection from "@/components/NewsletterSection";
+
+const Newsletter = () => {
+  return (
+    <main className="min-h-screen bg-background">
+      <div className="h-[100vh] flex items-center justify-center">
+        <NewsletterSection />
+      </div>
+    </main>
+  );
+};
+
+export default Newsletter;
